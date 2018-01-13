7ef08c22d11b86b9666b5a2adb1a3df5c85af328
Oskar Viljasaar <oskar.viljasaar@gmail.com>
2018-01-13 14:31:40 +0100

    eud'contrôler

---
+++ src/manet/DensityController.java
@@ -0,0 +1,12 @@
+package manet;
+
+import peersim.core.Control;
+import peersim.core.Network;
+import peersim.core.Node;
+
+public class DensityController implements Control {
+    @Override
+    public boolean execute() {
+        return false;
+    }
+}
