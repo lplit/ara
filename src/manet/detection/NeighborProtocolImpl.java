b0aaf6a23c2693360fcc6c58c6c017904e3df1e1
Oskar Viljasaar <oskar.viljasaar@gmail.com>
2018-01-13 15:50:41 +0100

    internal timers in NeighborProtocol

--- src/manet/detection/NeighborProtocolImpl.java
+++ src/manet/detection/NeighborProtocolImpl.java
@@ -19,17 +19,22 @@ public class NeighborProtocolImpl implements NeighborProtocol, EDProtocol {
     private int timer_delay;
     private int listener_pid;
 
+
+
     private static final String PAR_PERIOD = "period";
     private static final String PAR_TIMERDELAY = "timer_delay";
     private static final String PAR_LISTENER_PID = "listenerpid";
+    private static final String PAR_INTERNALTIMER = "internal_timer"; // 0 (par défaut) sans timers. 1 avec
     Integer timeStamp = 0;
 
     private List<Long> neighbor_list;
-    private Map<Node, Integer> neighbor_timers;
+
+    private int internal_timer = 0;
+    private Map<Long, Integer> neighbor_timers;
 
     public NeighborProtocolImpl(String prefix) {
         neighbor_list = new ArrayList<>();
-        neighbor_timers = new HashMap<Node, Integer>();
+        neighbor_timers = new HashMap<Long, Integer>();
 
         String tmp[]=prefix.split("\\.");
         this_pid= Configuration.lookupPid(tmp[tmp.length-1]);
@@ -37,6 +42,8 @@ public class NeighborProtocolImpl implements NeighborProtocol, EDProtocol {
         this.period = Configuration.getInt(prefix+"."+PAR_PERIOD);
         this.timer_delay = Configuration.getInt(prefix + "." + PAR_TIMERDELAY);
         this.listener_pid = Configuration.getPid(prefix + "." + PAR_LISTENER_PID,-1);
+        this.internal_timer = Configuration.getInt(prefix + "." + PAR_INTERNALTIMER, 0);
+        System.out.println("internal timer period " + this.period + " delay " + this.timer_delay + " internal " + this.internal_timer);
 
 
         }
@@ -68,53 +75,69 @@ public class NeighborProtocolImpl implements NeighborProtocol, EDProtocol {
 
 
         if (event instanceof Message) {
-//            System.err.println("Node " + node.getID() + " msg src " + msg.getIdSrc() + " dest " + msg.getIdDest() + " " + msg.getTag() + " " + msg.getContent());
+//           System.err.println("Node " + node.getID() + " msg src " + msg.getIdSrc() + " dest " + msg.getIdDest() + " " + msg.getTag() + " " + msg.getContent());
             /* on filtre déjà nos messages à nous
                 - "Heartbeat" (self-bootstrap)
                 - "Timer" pour déclencher les timers des gens
              */
-            if (msg.getIdDest() == -1) {
-                switch (msg.getTag()) {
-                    case "Heartbeat":
-                        EDSimulator.add(this.period, new Message(node.getID(), -1, "Heartbeat", "Heartbeat", this_pid), node, this_pid);
+
+            switch (msg.getTag()) {
+                case "Heartbeat":
+                if (msg.getIdDest() == -1) {
+                    EDSimulator.add(this.period, new Message(node.getID(), -1, "Heartbeat", "Heartbeat", this_pid), node, this_pid);
 //                        System.err.println(("recvd msg src" + msg.getIdSrc() + " dest " + msg.getIdDest()) + " " + msg.getTag() + neighbor_list);
-                        // salut y'a-t-il des nouveaux potos dans mon scope
+                    // salut y'a-t-il des nouveaux potos dans mon scope
 //                        System.err.println("emitting from neighbor");
-                        impl.emit(node, new Message(msg.getIdSrc(), -1, "Heartbeat", "Heartbeat", this_pid));
+                    impl.emit(node, new Message(msg.getIdSrc(), -1, "Heartbeat", "Heartbeat", this_pid));
 
+                    if (this.internal_timer != 0) {
+                        this.neighbor_timers.replaceAll((k, v) -> v - this.period);
+                    }
 
-                        break;
-                    case "Timer":
-                        if (neighbor_list.contains(msg.getContent())) {
-                            neighbor_list.remove(msg.getContent());
-                        }
-                        break;
                 }
-            } else { // du coup là on ne traite plus que les messages des autres
-                switch (msg.getTag()) {
-                    case "Heartbeat": // salutations mon nouveau poto
-                        if (!neighbor_list.contains(msg.getIdSrc())) {
-                            neighbor_list.add(msg.getIdSrc());
+                else if (msg.getIdDest() == node.getID()){ // heartbeat de quelqu'un d'autre
+                    System.err.println((node.getID() + ": recvd msg src" + msg.getIdSrc() + " dest " + msg.getIdDest()) + " " + msg.getTag() + neighbor_list);
+                    if (!neighbor_list.contains(msg.getIdSrc())) {
+                        neighbor_list.add(msg.getIdSrc());
+
+                        if (this.internal_timer != 0) {
+                            neighbor_timers.put(msg.getIdSrc(), this.timer_delay);
+
                             for (Long l : neighbor_list) {
+                                System.err.println("Adding node " + l);
                                 // pour tous les voisins dans la liste, on s'ajoute un timer
-                                EDSimulator.add(this.timer_delay, new Message(l, -1, "Timer", l, this_pid), node, this_pid);
+                                EDSimulator.add(this.timer_delay, new Message(-1, msg.getIdDest(), "Timer", l, this_pid), node, this_pid);
                             }
                         }
-                        //System.out.println(neighbor_list);
-                        break;
-                    case "Timer":
-                        System.err.println("TIMER should not be here");
-                        System.err.println("TIMER Node " + node.getID() + " msg src " + msg.getIdSrc() + " dest " + msg.getIdDest() + " " + msg.getTag() + " " + msg.getContent());
-                        if (neighbor_list.contains(msg.getContent())) {
-                            neighbor_list.remove(msg.getContent());
-                        }
-                        break;
-                    default:
-                        System.out.println("IN DEFAULT");
-                        break;
-                }
-            }
 
+                    }
+                }
+                else {
+                    System.err.println("Not supposed to be here");
+                }
+                break;
+                case "Timer":
+//                        System.err.println("TIMER should not be here");
+                    System.err.println("TIMER Node " + node.getID() + " msg src " + msg.getIdSrc() + " dest " + msg.getIdDest() + " " + msg.getTag() + " " + msg.getContent());
+                    if (this.internal_timer == 0) {
+                        if (neighbor_list.contains(msg.getContent())) {
+                        neighbor_list.remove(msg.getContent());
+                        }
+                    }
+                    else {
+                        if (neighbor_timers.containsKey(msg.getContent()))
+                            if (neighbor_timers.get(msg.getContent())  < 0) {
+                                System.err.println("Removing a node");
+                                neighbor_list.remove(msg.getContent());
+                                neighbor_timers.remove(msg.getContent());
+                            }
+                    }
+
+                break;
+                default:
+                    System.out.println("IN DEFAULT");
+                break;
+            }
         }
 
         return;
