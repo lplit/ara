fe1b3fe45c9c29a768dfeecd4df7531703044b3d
Oskar Viljasaar <oskar.viljasaar@gmail.com>
2018-01-27 01:23:42 +0100

    EmitterCounter + FloodingEmitter
    EmitterCounter: classe abstraite qui simplifie la vie + décompte le nombre de messages en transit. La classe concrète implémente la méthode emit()
    
    FloodingEmitter: classe concrète qui floode juste et qui émet les messages dans son rayon

---
+++ src/manet/communication/EmitterCounter.java
@@ -0,0 +1,79 @@
+package manet.communication;
+
+import manet.Message;
+import manet.positioning.PositionProtocol;
+import peersim.config.Configuration;
+import peersim.core.Network;
+import peersim.core.Node;
+import peersim.edsim.EDProtocol;
+import peersim.edsim.EDSimulator;
+
+import java.util.List;
+
+
+/** Décorateur sur Emitter qui compte le nombre de messages en transit.
+ *  La classe concrète s'occupe d'envoyer les messages, celle-ci s'occupe de la réception.
+ */
+
+public abstract class EmitterCounter implements Emitter, EDProtocol {
+
+    // Nombre de messages en transit.
+    private static int number_of_transits = 0;
+    protected Emitter emitter_impl;
+
+
+    protected int position_protocol;
+    protected int this_pid;
+
+
+    public EmitterCounter(String prefix, Emitter emitter) {
+        String tmp[]=prefix.split("\\.");
+        this_pid=Configuration.lookupPid(tmp[tmp.length-1]);
+
+        emitter_impl = emitter;
+
+        this.position_protocol= Configuration.getPid(prefix+"."+PAR_POSITIONPROTOCOL);
+
+    }
+
+    @Override
+    public void processEvent(Node node, int pid, Object event) {
+        if (pid == this_pid) {
+            number_of_transits--;
+        }
+    }
+
+    @Override
+    /** Cette méthode doit incrémenter number_of_transits au moment de l'émission.
+     */
+    public abstract void emit(Node host, Message msg);
+
+
+    @Override
+    public int getLatency() {
+        return emitter_impl.getLatency();
+    }
+
+    @Override
+    public int getScope() {
+        return emitter_impl.getScope();
+    }
+
+    public static int get_number_of_transits() {
+        return number_of_transits;
+    }
+
+
+    @Override
+    public Object clone(){
+        EmitterCounter res=null;
+        try {
+            res=(EmitterCounter) super.clone();
+        } catch (CloneNotSupportedException e) {}
+
+        res.emitter_impl.clone();
+        return res;
+    }
+//    public Node getParentNode();
+
+}
--- src/manet/communication/ProbabilisticEmitter.java
+++ src/manet/communication/ProbabilisticEmitter.java
@@ -17,11 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class ProbabilisticEmitter extends EmitterImpl implements  EmitterAtteignability, EDProtocol {
-    private static final String PAR_POSITIONPID = "positionprotocol";
-
-    private int position_pid;
-
+public class ProbabilisticEmitter extends EmitterCounter {
     // clé = msgid, valeur == initiateur du message
     public Map<Integer, Integer> messages_received;
 
@@ -31,7 +27,7 @@ public class ProbabilisticEmitter extends EmitterImpl implements  EmitterAtteign
         Map<Long, Position> res = new HashMap<>();
         for(int i = 0; i< Network.size(); i++) {
             Node n = Network.get(i);
-            PositionProtocol pos_proto_n = (PositionProtocol) n.getProtocol(position_pid);
+            PositionProtocol pos_proto_n = (PositionProtocol) n.getProtocol(position_protocol);
             Position cur = pos_proto_n.getCurrentPosition();
             res.put(n.getID(),cur);
         }
@@ -63,30 +59,17 @@ public class ProbabilisticEmitter extends EmitterImpl implements  EmitterAtteign
 
 
     public ProbabilisticEmitter(String prefix) {
-        super(prefix);
-        String tmp[]=prefix.split("\\.");
+        super(prefix, new EmitterImpl(prefix));
 
-        position_pid=Configuration.getPid(prefix+"."+PAR_POSITIONPID);
         probability = Configuration.getDouble(prefix+"." + PAR_PROBABILITY);
-
     }
 
     @Override
     public void emit(Node host, Message msg) {
         double _prob = CommonState.r.nextDouble();
         if (_prob < this.probability) {
-            super.emit(host, msg);
+            emitter_impl.emit(host, msg);
         }
-    }
-
-    @Override
-    public int getLatency() {
-        return super.getLatency();
-    }
-
-    @Override
-    public int getScope() {
-        return super.getScope();
     }
 
     @Override
@@ -94,25 +77,6 @@ public class ProbabilisticEmitter extends EmitterImpl implements  EmitterAtteign
         ProbabilisticEmitter res=null;
         res=(ProbabilisticEmitter) super.clone();
         return res;
-    }
-
-    @Override
-    public void processEvent(Node node, int pid, Object event) {
-        Message m;
-
-        if (pid != super.this_pid)
-            return;
-
-        m = (Message) event;
-
-        Integer msg_id = Integer.parseInt((String) m.getTag());
-        if (messages_received.get(msg_id) != null) {
-            // do nothing
-        }
-        else {
-            messages_received.put(msg_id, Integer.parseInt((String) m.getContent()));
-            this.emit(node, new Message(node.getID(), 0, m.getTag(), m.getContent(), this_pid));
-        }
     }
 
 }
