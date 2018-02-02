ed5b5b3b80775ebb08f0ba83cc03dbd1c5d7415f
Oskar Viljasaar <oskar.viljasaar@gmail.com>
2018-02-02 18:57:58 +0100

    InverseProportionalEmitter

--- src/manet/communication/algorithms/InverseProportionalEmitter.java
+++ src/manet/communication/algorithms/InverseProportionalEmitter.java
@@ -24,6 +24,8 @@ public class InverseProportionalEmitter extends EmitterCounter {
     @Override
     public void emit(Node host, Message msg) {
         number_of_sent = 0;
+        nodes_received.clear();
+        nodes_private.clear();
 
         double probability = k / get_neighbors_in_scope(host).size();
 
@@ -32,13 +34,17 @@ public class InverseProportionalEmitter extends EmitterCounter {
             for (Node n : get_neighbors_in_scope(host)) {
                 number_of_sent++;
                 has_finished = false;
+                nodes_private.add(n);
+            }
 
+            for (Node n: nodes_private) {
+                String new_tag = msg.getTag() + "//" + nodes_private;
                 EDSimulator.add(
                         getLatency(),
                         new Message(
                                 host.getID(),
                                 n.getID(),
-                                msg.getTag(),
+                                new_tag,
                                 msg.getContent(),
                                 msg.getPid()),
                         n,
@@ -46,6 +52,7 @@ public class InverseProportionalEmitter extends EmitterCounter {
                 if (verbose != 0)
                     System.err.println("Node " + host.getID() +
                             " InverseProportionalEmitter emitted w/" + _prob + "<" + probability + " sent " + number_of_sent + " messages");
+
 
             }
 
